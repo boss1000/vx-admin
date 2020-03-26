134e21e244e71a40828c87b4dadd094dd28768c5
xzb <1102059895@qq.com>
2020-03-26 18:04:24 +0800

    第一次提交

---
+++ src/views/approvalmanage/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>21313</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
---
+++ src/views/jurisdiction/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="commonTemp">
+    123
+  </div>
+</template>
+<script>
+export default {
+  name: 'Jurisdiction',
+  data() {
+    return {
+    };
+  },
+  mounted() {
+    console.log(344)
+  }
+};
+</script>
+<style lang="scss" scoped>
+.tagclose {
+  /deep/ .el-tag__close.el-icon-close {
+    display: none;
+    position: absolute;
+  }
+}
+</style>
